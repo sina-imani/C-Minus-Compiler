7895a9336f0c950b94f5c6e08c5909d316f382a0
sina <mr.sinaimani@gmail.com>
2023-06-07 21:47:26 +0330

    make compiler.py flush PB into output at the end

--- compiler.py
+++ compiler.py
@@ -2,14 +2,14 @@
 
 import parser
 import scanner
+import code_maker
 
 # Setting files
 try:
     scanner.INPUT_FILE = open('input.txt')
-    parser.TREE_FILE = open('parse_tree.txt', 'w')
-    parser.SYNTAX_ERROR_FILE = open('syntax_errors.txt', 'w')
-
     scanner.init_symbol_table()
+    code_maker_file = open('output.txt')
+
 except (FileNotFoundError, PermissionError):
     print('Error while configuring files: No input.txt or insufficient process access')
     exit(-1)
@@ -23,6 +23,11 @@ try:
     scanner.INPUT_FILE.close()
     parser.TREE_FILE.close()
     parser.SYNTAX_ERROR_FILE.close()
+    for i in range(len(code_maker.PB) - 1):
+        code_maker_file.write(code_maker.PB[i] + '\n')
+    if len(code_maker.PB) > 0:
+        code_maker_file.write(code_maker.PB[-1])
+    code_maker_file.close()
 
 except IOError:
     print('Error while closing files.')
